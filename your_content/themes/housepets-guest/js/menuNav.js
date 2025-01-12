422ddbc87c05d093e6761621caf7cb86cf0c0e35
mothorialcomic <65144263+mothorialcomic@users.noreply.github.com>
2025-01-12 18:05:00 -0500

    guest

---
+++ your_content/themes/housepets-guest/js/menuNav.js
@@ -0,0 +1,30 @@
+export function showOverlay(comicWorld) {
+    const overlayDiv = comicWorld + "Overlay";
+    const centeredDiv = document.getElementById(overlayDiv); // Get the centeredDiv by ID
+    overlay.style.visibility = 'visible';
+    centeredDiv.style.visibility = 'visible';
+}
+export function hideOverlay(comicWorld) {
+    const overlayDiv = comicWorld + "Overlay";
+    const centeredDiv = document.getElementById(overlayDiv); // Get the centeredDiv by ID
+    overlay.style.visibility = 'hidden';
+    centeredDiv.style.visibility = 'hidden';
+}
+
+function revealMenu() {
+	var element = document.getElementById("links-menu");
+	element.classList.toggle("menuHide");
+	var menuBar = document.getElementById("mobileMenu");
+	menuBar.classList.toggle("menuToggled");
+	var oneCycle = document.getElementsByClassName("ranOnce").length;
+	var text = menuBar.innerHTML;
+	if (text == "Show Menu" || oneCycle == 0) {
+		menuBar.innerHTML = "Hide Menu";
+	} else {
+		menuBar.innerHTML = "Show Menu";
+	}
+	
+	if (oneCycle == 0) {
+	menuBar.classList.toggle("ranOnce");
+	}
+}
